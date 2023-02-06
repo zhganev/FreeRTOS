ee9b0f73d5ff8882c27185e271d36a39a9c20078
Georgi Zhganev <zhganev@icloud.com>
2023-02-06 23:37:53 +0200

    2 task cooperative scheduling implemented

--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -10,19 +10,32 @@ void setup() {
   TaskHandle_t task2_handle;
   BaseType_t status;
 
+  Serial.begin(9600);
+
   status = xTaskCreate(task1_handler, "Task-1", 200, "Hello World from Task-1", 2, &task1_handle);
   ;
   status = xTaskCreate(task2_handler, "Task-2", 200, "Hello World from Task-2", 2, &task2_handle);
   ;
+
+  /*Start the FreeRTOS Scheduler*/
+  vTaskStartScheduler();
+
+  /*If control reaches here, than the launch of the scheduler failed due to insufficient memory in heap*/
 
 }
 
 void loop() {}
 
 static void task1_handler(void* parameters){
-
+  while(1){
+    Serial.println((char*) parameters);
+    vPortYield();
+  }
 }
 
 static void task2_handler(void* parameters){
-
+  while(1){
+    Serial.println((char*) parameters);
+    vPortYield();
+  }
 }
--- build/sketch/main.ino.cpp
+++ build/sketch/main.ino.cpp
@@ -7,7 +7,7 @@ static void task2_handler(void* parameters);
 
 #line 6 "/Users/georgi/GitHub/FreeRTOS/main.ino"
 void setup();
-#line 19 "/Users/georgi/GitHub/FreeRTOS/main.ino"
+#line 26 "/Users/georgi/GitHub/FreeRTOS/main.ino"
 void loop();
 #line 6 "/Users/georgi/GitHub/FreeRTOS/main.ino"
 void setup() {
@@ -16,19 +16,32 @@ void setup() {
   TaskHandle_t task2_handle;
   BaseType_t status;
 
+  Serial.begin(9600);
+
   status = xTaskCreate(task1_handler, "Task-1", 200, "Hello World from Task-1", 2, &task1_handle);
   configASSERT(status == pdPASS);
   status = xTaskCreate(task2_handler, "Task-2", 200, "Hello World from Task-2", 2, &task2_handle);
   configASSERT(status == pdPASS);
+
+  /*Start the FreeRTOS Scheduler*/
+  vTaskStartScheduler();
+
+  /*If control reaches here, than the launch of the scheduler failed due to insufficient memory in heap*/
 
 }
 
 void loop() {}
 
 static void task1_handler(void* parameters){
-
+  while(1){
+    Serial.println((char*) parameters);
+    taskYIELD();
+  }
 }
 
 static void task2_handler(void* parameters){
-
+  while(1){
+    Serial.println((char*) parameters);
+    taskYIELD();
+  }
 }
