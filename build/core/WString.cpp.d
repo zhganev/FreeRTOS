58f06da20bbf5b99aa40df69f45a9063e17b2c04
zhganev <zhganev@icloud.com>
2023-02-13 14:37:38 +0200

    test successfull on target

--- build/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-/Users/georgi/GitHub/FreeRTOS/build/core/WString.cpp.o: \
- /Users/georgi/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/WString.cpp \
- /Users/georgi/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/WString.h
--- build/core/wiring_digital.c.d
+++
@@ -1,6 +0,0 @@
-/Users/georgi/GitHub/FreeRTOS/build/core/wiring_digital.c.o: \
- /Users/georgi/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/wiring_digital.c \
- /Users/georgi/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/wiring_private.h \
- /Users/georgi/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Arduino.h \
- /Users/georgi/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/binary.h \
- /Users/georgi/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/variants/standard/pins_arduino.h
